f7a232b794ac8315e350c402ee151fce44c63bd5
Amir Savand <amir@savandbros.com>
2019-06-28 23:39:43 +0300

    Initiate Angular 8 :diamonds:

--- app/app.module.js
+++
@@ -1,9 +0,0 @@
-let app = angular.module("mr-player", [
-  "ngAnimate",
-  "toaster",
-  "youtube-embed",
-  "angular-loading-bar",
-  "angular-google-analytics",
-  "angular.filter",
-  "ui.router",
-]);
