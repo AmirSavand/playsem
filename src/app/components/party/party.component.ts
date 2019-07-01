1e80c30d5ad5a7d7f115c0446cca0b469e3b4bab
Amir Savand <amir@savandbros.com>
2019-07-01 22:47:21 +0300

    Get party and songs

--- src/app/components/party/party.component.ts
+++ src/app/components/party/party.component.ts
@@ -1,15 +1,67 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Party } from '@app/interfaces/party';
+import { Song } from '@app/interfaces/song';
+import { ApiService } from '@app/services/api/api-service.service';
 
 @Component({
   selector: 'app-party',
   templateUrl: './party.component.html',
-  styleUrls: ['./party.component.scss']
+  styleUrls: ['./party.component.scss'],
 })
 export class PartyComponent implements OnInit {
 
-  constructor() { }
+  /**
+   * Party ID from param
+   */
+  partyId: string;
 
-  ngOnInit() {
+  /**
+   * Party data
+   */
+  party: Party;
+
+  /**
+   * Song list of party
+   */
+  songs: Song[];
+
+  constructor(private api: ApiService,
+              private route: ActivatedRoute) {
   }
 
+  ngOnInit(): void {
+    /**
+     * Watch param changes
+     */
+    this.route.paramMap.subscribe(params => {
+      /**
+       * Get party ID from params
+       */
+      this.partyId = params.get('partyId');
+      /**
+       * Load party data
+       */
+      this.api.getParty(this.partyId).subscribe(data => {
+        this.party = data;
+        /**
+         * Load party songs
+         */
+        this.loadSongs();
+      });
+    });
+  }
+
+  /**
+   * Load songs of party
+   */
+  loadSongs(): void {
+    this.api.getSongs(this.party.id).subscribe(data => {
+      this.songs = data.results;
+      // Set party of each song
+      for (const song of this.songs) {
+        song.party = this.party;
+      }
+    });
+  }
 }
