facdc32d8de6c6f6e5142393dc67b3ffc0210055
Amir Savand <amir@savandbros.com>
2019-07-01 22:13:09 +0300

    Add getSongs to API

--- src/app/services/api/api-service.service.ts
+++ src/app/services/api/api-service.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ApiResponse } from '@app/interfaces/api-response';
 import { Party } from '@app/interfaces/party';
+import { Song } from '@app/interfaces/song';
 import { environment } from '@environments/environment';
 import { Observable } from 'rxjs';
 
@@ -23,5 +24,14 @@ export class ApiService {
    */
   getParties(): Observable<ApiResponse<Party>> {
     return this.http.get<ApiResponse<Party>>(`${ApiService.base}parties`).pipe();
+  }
+
+  /**
+   * Get songs
+   */
+  getSongs(party: string): Observable<ApiResponse<Song>> {
+    return this.http.get<ApiResponse<Song>>(`${ApiService.base}songs`, {
+      params: { party },
+    }).pipe();
   }
 }
