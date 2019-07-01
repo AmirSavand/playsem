e79ce236a8a41c311b13d34a45b2e33dff1583c0
Amir Savand <amir@savandbros.com>
2019-07-01 21:36:31 +0300

    Load parties in dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Party } from '@app/interfaces/party';
+import { ApiService } from '@app/services/api/api-service.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  parties: Party[];
 
-  ngOnInit() {
+  constructor(private api: ApiService) {
   }
 
+  ngOnInit(): void {
+    this.api.getParties().subscribe(data => {
+      this.parties = data.results;
+    });
+  }
 }
