5cef4ac584ccc694464816f6831113ef344bdf85
Arsalan Savand <arsalan@savandbros.com>
2019-07-05 14:04:59 +0300

    Add a variable to toggle sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,18 @@ import { AuthService } from '@app/services/auth/auth.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
+
 export class AppComponent implements OnInit {
 
   /**
    * Authenticated user
    */
   user: User;
+
+  /**
+   * Indicates whether sidebar is closed or not
+   */
+  isClosed: boolean;
 
   constructor(public auth: AuthService) {
   }
