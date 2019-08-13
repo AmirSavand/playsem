96e8b5d9af5592f1c35b5e95b071a9c12fa09be3
Amir Savand <amir@savandbros.com>
2019-08-13 16:48:13 +0300

    Create implementing.service.ts

---
+++ src/app/shared/implementing/implementing.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ImplementingService {
+
+  constructor() {
+  }
+
+  /**
+   * Show alert about not implemented feature
+   *
+   * @param feature Feature name
+   */
+  static alert(feature: string = 'This feature'): void {
+    alert(`${feature} is not implemented yet.`);
+  }
+}
