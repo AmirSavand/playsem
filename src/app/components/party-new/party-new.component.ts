878fa647c12b8c0c396c18de21b95f1d5fef45f5
Amer Ansari <amer@savandbros.com>
2019-09-11 01:10:34 +0400

    Add cover & image to new party

--- src/app/components/party-new/party-new.component.ts
+++ src/app/components/party-new/party-new.component.ts
@@ -46,6 +46,8 @@ export class PartyNewComponent implements OnInit {
     this.partyForm = this.formBuilder.group({
       title: [''],
       description: [''],
+      image: [''],
+      cover: [''],
     });
   }
 
