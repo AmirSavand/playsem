e6f525240534dab3a95a36cec4269f6216db69fa
Amer Ansari <amer@savandbros.com>
2019-10-11 13:45:12 +0400

    Get user details & show them

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Party } from '@app/interfaces/party';
+import { User } from '@app/interfaces/user';
+import { ApiService } from '@app/services/api/api-service.service';
+import { AuthService } from '@app/services/auth/auth.service';
+import { PartyService } from '@app/services/party/party.service';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']
+  styleUrls: ['./user.component.scss'],
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  /**
+   * Authenticated user
+   */
+  user: User;
 
-  ngOnInit() {
+
+  constructor(private auth: AuthService) {
   }
 
+  ngOnInit(): void {
+    /**
+     * Get authenticated user data and watch for changes
+     */
+    this.auth.user.subscribe(data => {
+      this.user = data;
+    });
+  }
 }
