1ec37d472a615060f4498431b5a59541d9a2001a
Amir Savand <amir@savandbros.com>
2019-12-27 17:58:34 +0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,22 +6,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ApiService } from '@app/services/api/api-service.service';
 import { AuthInterceptorService } from '@app/services/auth-interceptor/auth-interceptor.service';
 import { AuthService } from '@app/services/auth/auth.service';
-
 import { CategoryModalComponent } from '@app/shared/category-modal/category-modal.component';
+import { ImplementingComponent } from '@app/shared/implementing/implementing.component';
 import { PlayerComponent } from '@app/shared/player/player.component';
+import { SongModalComponent } from '@app/shared/song-modal/song-modal.component';
 import { environment } from '@environments/environment';
-
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
 import { LoadingBarModule } from '@ngx-loading-bar/core';
 import { LoadingBarHttpClientModule } from '@ngx-loading-bar/http-client';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
-
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { PopoverModule } from 'ngx-bootstrap/popover';
 import { ProgressbarModule } from 'ngx-bootstrap/progressbar';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
-
 import { CookieService } from 'ngx-cookie-service';
 import { NgxGoogleAnalyticsModule, NgxGoogleAnalyticsRouterModule } from 'ngx-google-analytics';
 import { NgxMdModule } from 'ngx-md';
@@ -30,8 +27,6 @@ import { NgxY2PlayerModule } from 'ngx-y2-player';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ImplementingComponent } from './shared/implementing/implementing.component';
-import { SongModalComponent } from './shared/song-modal/song-modal.component';
 
 @NgModule({
   declarations: [
@@ -67,11 +62,7 @@ import { SongModalComponent } from './shared/song-modal/song-modal.component';
     SongModalComponent,
   ],
   providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptorService,
-      multi: true,
-    },
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true },
     CookieService,
     ApiService,
     AuthService,
